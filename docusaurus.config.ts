92abaa12fb86ba4ece64812bc5bda192d28096e6
rikkarth <ricardominamendes@outlook.com>
2023-12-10 16:00:25 +0000

    updated deploy config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://rikkarth.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'codeforge', // Usually your GitHub org/user name.
-  projectName: 'tooler-tools', // Usually your repo name.
+  organizationName: 'rikkarth', // Usually your GitHub org/user name.
+  projectName: 'tooler-tools-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
